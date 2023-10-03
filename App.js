9d692e88648c389ba4d12c273d021b06ac9c35ff
Luanoly <los8@aluno.ifal.edu.br>
2023-10-03 14:29:19 -0300

    criação de menu

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Oopenheimer</Text>
+      <View style={styles.menuBar}>
+        <TouchableOpacity style={styles.menuItem}>
+          <Text>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <Text>Ajuda</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <Text>Contato</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.content}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,6 +28,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  menuBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#ccc',
+    height: 50,
+  },
+  menuItem: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  content: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
