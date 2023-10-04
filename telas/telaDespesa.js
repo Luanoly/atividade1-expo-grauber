f2fa2cf455101722215179fc4953a6dada93b810
Luanoly <los8@aluno.ifal.edu.br>
2023-10-04 16:07:13 -0300

    adicionando caixa de texto para aparecer valor

--- telas/telaDespesa.js
+++ telas/telaDespesa.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, StatusBar } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, StatusBar, TextInput, Button } from 'react-native';
 
 const TelaDespesa = () => {
+    const [valor, setValor] = useState(''); // Estado para armazenar o valor inserido
+
+    // Função para atualizar o estado com o valor inserido
+    const handleValorChange = (text) => {
+        setValor(text);
+    };
+
     return (
         <View style={styles.container}>
             <StatusBar style="auto" />
@@ -10,9 +17,30 @@ const TelaDespesa = () => {
                     <Text style={{ color: 'white', fontSize: 24 }}>PoupCerto</Text>
                 </TouchableOpacity>
             </View>
+
+            <View style={styles.content}>
+                <Text style={{ color: 'black', marginTop: 40, fontSize: 20 }}>Gerencie suas finanças com eficiência e acompanhe suas despesas diárias de forma inteligente.</Text>
+
+                {/* Caixa de texto para inserir o valor */}
+                <TextInput
+                    placeholder="Insira o valor"
+                    style={styles.input}
+                    onChangeText={handleValorChange}
+                    value={valor}
+                />
+
+                {/* Exibir o valor inserido */}
+                <Text style={styles.valorExibido}>Valor inserido: {valor}</Text>
+
+                {/* Botão para limpar o valor */}
+                <Button
+                    title="Limpar"
+                    onPress={() => setValor('')}
+                />
+            </View>
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
@@ -26,15 +54,24 @@ const styles = StyleSheet.create({
         backgroundColor: '#4682b4',
         height: 70,
     },
-    texto: {
-        flex: 0.5,
+    content: {
+        flex: 1,
         alignItems: 'center',
         justifyContent: 'flex-start',
-        backgroundColor: 'white',
+        backgroundColor: '#fff',
+        padding: 20,
     },
-    Calendar: {
+    input: {
+        width: '100%',
+        height: 40,
         borderWidth: 1,
-        borderColor: '#4682b4'
+        borderColor: '#ccc',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10,
+    },
+    valorExibido: {
+        fontSize: 18,
     },
 });
 
