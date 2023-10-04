21808ea2c56a4cbf09af35b8696801b591ab8dfb
Luanoly <los8@aluno.ifal.edu.br>
2023-10-04 13:55:10 -0300

    ajustando nomes e anuncio

--- App.js
+++ App.js
@@ -8,13 +8,7 @@ export default function App() {
       <StatusBar style="auto" />
       <View style={[styles.menuBar, { marginTop: 0 }]}>
         <TouchableOpacity style={styles.menuItem}>
-          <Text style={{ color: 'white', fontSize: 18 }}>Home</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
           <Text style={{ color: 'white', fontSize: 24 }}>PoupCerto</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.menuItem}>
-          <Text style={{ color: 'white', fontSize: 18 }}>Contato</Text>
         </TouchableOpacity>
       </View>
 
@@ -22,20 +16,17 @@ export default function App() {
         <Text style={{ color: 'black', marginTop: 30, fontSize: 20, color: '#4682b4' }}>ULTIMOS REGISTROS DO MÊS</Text>
       </View>
 
-      <View style={styles.botoes}>
-        <Button title='mes'></Button>
-        <Button title='ano' ></Button>
-      </View>
-
       <View>
         <Calendar style={styles.Calendar} />
       </View>
 
       <View style={styles.anuncio}>
-        <Text style={{ color: 'black', marginTop: 30, fontSize: 20, backgroundColor: 'yellow' }}>ANUNCIO</Text>
+        <Text style={{ color: 'black', marginTop: 30, fontSize: 20 }}>ANUNCIO</Text>
       </View>
 
-      <Button title='Poupar' color={'green'} ></Button>
+      <View style={styles.poupar}>
+        <Button title='Poupar' color={'green'} ></Button>
+      </View>
 
     </View>
   );
@@ -53,23 +44,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#4682b4',
     height: 70,
   },
-  menuItem: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   texto: {
     flex: 0.5,
     alignItems: 'center',
     justifyContent: 'flex-start',
-    backgroundColor: 'white',
-  },
-  botoes: {
-    flex: 0.5,
-    alignItems: 'flex-start',
-    justifyContent: 'space-around',
-    flexDirection: 'row',
-    marginTop: 10,
     backgroundColor: 'white',
   },
   Calendar: {
@@ -78,6 +56,16 @@ const styles = StyleSheet.create({
   },
   anuncio: {
     flex: 2,
+    alignItems: 'center',
+    backgroundColor: 'yellow'
+  },
+  poupar: {
+    flex: 0.3,
+    margin: 20,
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: 'green',
   }
 });
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
