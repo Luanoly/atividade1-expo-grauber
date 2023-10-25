381f6c9ce6088a3f304e5d54aba59f967d5a0231
luan 25 Out <los8@aluno.ifal.edu.br>
2023-10-25 14:43:44 -0300

    ajustando tela inicial

--- telas/TelaInicial.js
+++ telas/TelaInicial.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, StatusBar, Button } from 'react-native';
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
 const TelaInicial = () => {
     return (
         <View style={styles.container}>
-            <StatusBar style="auto" />
-            <View style={[styles.menuBar, { marginTop: 0 }]}>
-                <TouchableOpacity style={styles.menuItem}>
-                    <Text style={{ color: 'white', fontSize: 24 }}>PoupCerto</Text>
+            <View style={[styles.telaSuperior, { marginTop: 0 }]}>
+                <TouchableOpacity>
+                    <Text style={{ color: 'white', fontSize: 28, marginLeft: 24, marginBottom: 40 }}>PoupCerto</Text>
                 </TouchableOpacity>
+
             </View>
 
-            <View style={styles.texto}>
-                <Text style={{ color: 'black', marginTop: 30, fontSize: 20, color: '#4682b4' }}>ULTIMOS REGISTROS DO MÊS</Text>
+            <View style={styles.textoMeio}>
+                <Text style={{ color: 'black', marginTop: 30, fontSize: 20, color: '#6AAAE6', }}>ULTIMOS REGISTROS DO MÊS</Text>
             </View>
 
         </View>
@@ -21,19 +20,26 @@ const TelaInicial = () => {
 }
 
 const styles = StyleSheet.create({
+    // View principal não mexer
     container: {
         flex: 1,
         backgroundColor: 'white',
     },
-    menuBar: {
+    telaSuperior: {
+        flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center',
-        backgroundColor: '#4682b4',
+        backgroundColor: '#6AAAE6',
         height: 70,
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        borderColor: 'white',
+        borderBottomWidth: 2,
+        borderStyle: 'solid'
     },
-    texto: {
-        flex: 0.5,
+    textoMeio: {
+        flex: 3.5,
         alignItems: 'center',
         justifyContent: 'flex-start',
         backgroundColor: 'white',
