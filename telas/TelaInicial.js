2da4544d73a64b71df36d121829ab896dc35b718
Luanoly <los8@aluno.ifal.edu.br>
2023-10-26 23:09:00 -0300

    mudando cor '#082D15'

--- telas/TelaInicial.js
+++ telas/TelaInicial.js
@@ -10,7 +10,7 @@ const TelaInicial = () => {
                 <View>
                     <Text style={{ color: 'white', fontSize: 28, marginLeft: 24, marginTop: 40 }}>PoupCerto</Text>
                     <Text style={{ color: 'white', fontSize: 36, marginLeft: 24, marginTop: 10 }}>R$: 1.236,43 
-                    <Avatar.Icon size={28} color='white' backgroundColor='#6AAAE6' icon="eye" />
+                    <Avatar.Icon size={28} color='white' backgroundColor='#0A3318' icon="eye" />
                     </Text>
                 </View>
             </View>
@@ -18,41 +18,41 @@ const TelaInicial = () => {
             <View style={styles.telaInicial}>
                 <View style={styles.componenteInicial}>
                     <Text style={styles.fontePadrao}>Histórico</Text>
-                    <Avatar.Icon size={120} backgroundColor='#6AAAE6' icon="file-document-multiple-outline"/>
+                    <Avatar.Icon size={120} backgroundColor='#134F29' icon="file-document-multiple-outline"/>
                 </View>
                 <View style={styles.componenteInicial}>
                     <Text style={styles.fontePadrao}>Cofre</Text>
-                    <Avatar.Icon size={120} backgroundColor='#6AAAE6' icon="piggy-bank-outline"/>
+                    <Avatar.Icon size={120} backgroundColor='#134F29' icon="piggy-bank-outline"/>
                 </View>
             </View>
 
             <View style={styles.telaUltimoHistorico}>
                 <View style={ styles.registroHistorico }>
-                    <Avatar.Icon size={54} color='#6AAAE6' backgroundColor='white' icon="ambulance" />
+                    <Avatar.Icon size={54} color='#134F29' backgroundColor='white' icon="ambulance" />
                     <Text style={styles.fontePadrao}>Farmácia</Text>
                     <Text style={styles.fontePadrao}>R$: 300,00</Text>
                 </View>
                 <View style={ styles.registroHistorico}>
-                    <Avatar.Icon size={54} color='#6AAAE6' backgroundColor='white' icon="cart-arrow-down" />
+                    <Avatar.Icon size={54} color='#134F29' backgroundColor='white' icon="cart-arrow-down" />
                     <Text style={styles.fontePadrao}>Mercado</Text>
                     <Text style={styles.fontePadrao}>R$: 36,00</Text>
                 </View>
                 <View style={ styles.registroHistorico}>
-                    <Avatar.Icon size={54} color='#6AAAE6' backgroundColor='white' icon="credit-card" />
+                    <Avatar.Icon size={54} color='#134F29' backgroundColor='white' icon="credit-card" />
                     <Text style={styles.fontePadrao}>Cartões</Text>
                     <Text style={styles.fontePadrao}>R$: 209,00</Text>
                 </View>
                 <View style={ styles.registroHistorico}>
-                    <Avatar.Icon size={54} color='#6AAAE6' backgroundColor='white' icon="package" />
+                    <Avatar.Icon size={54} color='#134F29' backgroundColor='white' icon="package" />
                     <Text style={styles.fontePadrao}>Outros</Text>
                     <Text style={styles.fontePadrao}>R$: 100,00</Text>
                 </View>
             </View>
 
             <View style={styles.menu}>
-                <Avatar.Icon size={52} color='white' backgroundColor='#6AAAE6' icon="home" />
-                <Avatar.Icon size={90} color='white' backgroundColor='#6AAAE6' borderWidth={1} borderColor='grey' icon="cash-plus" />
-                <Avatar.Icon size={52} color='white' backgroundColor='#6AAAE6' icon="bug" />
+                <Avatar.Icon size={52} color='white' backgroundColor='#082D15' icon="home" />
+                <Avatar.Icon size={90} color='white' backgroundColor='#082D15' borderWidth={1} borderColor='grey' icon="cash-plus" />
+                <Avatar.Icon size={52} color='white' backgroundColor='#082D15' icon="bug" />
             </View>
 
         </View>
@@ -70,7 +70,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center',
-        backgroundColor: '#6AAAE6',
+        backgroundColor: '#0A3318',
         height: 70,
         flexDirection: 'column',
         alignItems: 'flex-start',
@@ -86,7 +86,7 @@ const styles = StyleSheet.create({
         margin: 4,
     },
     componenteInicial: {
-        backgroundColor: '#6AAAE6',
+        backgroundColor: '#134F29',
         width: '40%',
         alignItems: 'center',
         height: '90%',
@@ -95,14 +95,14 @@ const styles = StyleSheet.create({
     telaUltimoHistorico: {
         flex: 2,
         borderWidth: 12,
-        borderColor: '#6AAAE6',
+        borderColor: '#134F29',
         borderRadius: 4,
         padding: 10,
         margin: 8,
         justifyContent: 'space-around'
     },
     registroHistorico: {
-        backgroundColor: '#6AAAE6',
+        backgroundColor: '#134F29',
         flexDirection: 'row',
         padding: 10,
         alignItems: 'center',
@@ -115,7 +115,7 @@ const styles = StyleSheet.create({
         flex: 0.3,
         borderTopWidth: 2,
         borderColor: 'white',
-        backgroundColor: '#6AAAE6',
+        backgroundColor: '#082D15',
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center'
