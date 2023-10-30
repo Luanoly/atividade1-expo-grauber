1f2dcaaedc488988ff9fc16670ff5df8657be046
Luanoly <los8@aluno.ifal.edu.br>
2023-10-30 00:13:55 -0300

    alterando fonte

--- telas/telaDespesa.js
+++ telas/telaDespesa.js
@@ -7,9 +7,13 @@ const TelaDespesa = () => {
         <View style={styles.container}>
 
             <View style={[styles.telaSuperior]}>
-                <View>
-                    <Text style={{ color: 'white', fontSize: 32, marginLeft: 24, marginTop: 40 }}>PoupCerto</Text>
-                    <Text style={{ color: 'white', fontSize: 28, marginLeft: 24, marginTop: 10 }}>R$: 1.236,43</Text>
+             <Text style={{ color: '#fff', fontSize: 20, marginLeft: 24, marginTop: 40 }}>PoupCerto</Text>
+                <View style={styles.telaDespesaValor}>
+                    <Text style={{ color: '#fff', fontSize: 18, marginLeft: 24, marginTop: 4 }}>Despesa:</Text>
+                     <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                       <Text style={{ color: '#fff', fontSize: 32, marginLeft: 24, marginTop: 4 }}>R$: 645,00</Text>
+                       <Avatar.Icon size={28} color='#fff' backgroundColor='#003B45' icon="eye" />
+                     </View>
                 </View>
             </View>
 
@@ -20,10 +24,7 @@ const TelaDespesa = () => {
                 <View sytle={styles.atributosDespesas}>
                     <View style={styles.colunaDespesaUm}>
                         <TextInput label="R$: 00,00" />
-
-                        {/* <Select placeholder='Descrição'>
-                            <Select.Item label="UX" value="ux" />
-                        </Select> */}
+                        <Text>Categorías</Text>
                     </View>
                 </View>
             </View>
@@ -33,9 +34,15 @@ const TelaDespesa = () => {
             </View>
 
             <View style={styles.menu}>
-                <Avatar.Icon size={52} color='#798899' backgroundColor='#fff' icon="home" />
-                <Avatar.Icon size={90} color='#798899' backgroundColor='#fff' borderWidth={1} borderColor='grey' icon="cash-plus" />
-                <Avatar.Icon size={52} color='#798899' backgroundColor='#fff' icon="bug" />
+                <View style={ styles.iconesMenu}>
+                    <Avatar.Icon size={38} color='#798899' backgroundColor='#fff' icon="home" />
+                    <Text style={{color: '#798899'}}>Início</Text>
+                </View>
+                    <Avatar.Icon size={90} color='#AA0014' backgroundColor='#fff' borderWidth={1} borderColor='#AA0014' icon="cash-plus" />
+                <View style={ styles.iconesMenu}>
+                    <Avatar.Icon size={38} color='#798899' backgroundColor='#fff' icon="bug" />
+                    <Text style={{color: '#798899'}}>Sobre</Text>
+                </View>
             </View>
 
         </View>
@@ -49,21 +56,31 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
     },
     telaSuperior: {
-        flex: 1,
+        flex: 1.1,
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center',
-        backgroundColor: '#6AAAE6',
-        height: 70,
+        backgroundColor: '#004B57',
         flexDirection: 'column',
         alignItems: 'flex-start',
-        borderColor: 'white',
-        borderBottomWidth: 2,
-        borderStyle: 'solid'
+        borderColor: '#798899',
+        borderBottomWidth: 0.5,
+        borderStyle: 'solid',
+    },
+    telaDespesaValor: {
+        backgroundColor: '#003B45',
+        borderBottomWidth: 0.9,
+        borderColor: '#798899',
+        borderRadius: 4,
+        height: 90,
+        width: 370,
+        marginLeft: 8,
+        marginBottom: 10,
+        alignItems: 'flex-start'
     },
     inserirDespesa: {
-        flex: 2,
-        backgroundColor: '#6AAAE6',
+        flex: 1.6,
+        backgroundColor: '#003B45',
         borderRadius: 4,
         padding: 10,
         margin: 8,
@@ -78,19 +95,27 @@ const styles = StyleSheet.create({
     descricao: {
         flex: 1,
         borderWidth: 4,
-        borderColor: '#6AAAE6',
+        borderColor: '#003B45',
         borderRadius: 4,
         padding: 4,
         margin: 12,
     },
     menu: {
-        flex: 0.3,
-        borderTopWidth: 1,
-        borderColor: 'grey',
+        flex: 0.32,
+        borderTopWidth: 0.5,
+        borderColor: '#798899',
         backgroundColor: '#fff',
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center'
+    },
+    iconesMenu: {
+        alignItems: 'center',
+        height: 60
+    },
+    fontePadrao: {
+        fontSize: 20,
+         color: '#fff'
     },
 });
 
