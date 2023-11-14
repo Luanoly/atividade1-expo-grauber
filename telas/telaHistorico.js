53e90b1880876e4775d54d22bb9490d3981e4c02
Luanoly <los8@aluno.ifal.edu.br>
2023-11-14 16:26:03 -0300

    Criando tela hisotico

---
+++ telas/telaHistorico.js
@@ -0,0 +1,22 @@
+import { View } from "native-base";
+
+const TelaHistorico = () => {
+    return (
+        <View style={styles.container}>
+
+            <View>
+                <Text>PoupCerto</Text>
+            </View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+});
+
+export default TelaHistorico;
