02f7bbd68efb3a04d1c5adf2bf70daedac37b42b
Shara5904 <128625731+Shara5904@users.noreply.github.com>
2023-12-18 13:33:42 -0300

    testes unitarios (INFERNO)

--- telas/test.js
+++ telas/test.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
-import Despesa from './Despesa'; // Importe seu componente Despesa
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import TelaDespesa from './TelaDespesa';
+import fetchMock from 'jest-fetch-mock';
 
-describe('Despesa Component', () => {
-    test('renders Despesa component with correct props', () => {
-        const { getByText } = render(<Despesa categoria="Comida" valor={50} />);
-
-        // Verifica se o componente está renderizado com os valores corretos
-        expect(getByText('Comida')).toBeTruthy();
-        expect(getByText('R$50,00')).toBeTruthy();
+describe('TelaDespesa Component', () => {
+    beforeEach(() => {
+        fetchMock.resetMocks();
     });
 
-    test('onPress calls detalhes function', () => {
-        const detalhesMock = jest.fn(); // Cria uma função mock para detalhes
-        const { getByText } = render(<Despesa categoria="Comida" valor={50} detalhes={detalhesMock} />);
+    test('envia os dados da despesa ao chamar a função sendDespesa', async () => {
+        const { getByPlaceholderText, getByText } = render(<TelaDespesa />);
 
-        // Simula o pressionamento do componente
-        fireEvent.press(getByText('Comida R$50,00'));
+        const inputValor = getByPlaceholderText('Inserir valor');
+        const inputDescricao = getByPlaceholderText('Descrição...');
+        const buttonAdicionar = getByText('Adicionar');
 
-        // Verifica se a função detalhes foi chamada corretamente
-        expect(detalhesMock).toHaveBeenCalled();
+        // Aqui preenche os input's
+        fireEvent.changeText(inputValor, '100');
+        fireEvent.changeText(inputDescricao, 'Descrição da despesa');
+
+        // Simalução da 'função' fetch
+        fetchMock.mockResponseOnce(JSON.stringify({ success: true }));
+
+        // Simulando o clique do botão de adicionar
+        fireEvent.press(buttonAdicionar);
+
+        // Esperando a chamada da função
+        await waitFor(() => {
+            expect(fetchMock).toHaveBeenCalledWith(
+                'http://10.220.30.122:3000/despesas',
+                expect.objectContaining({
+                    method: 'POST',
+                    body: JSON.stringify({
+                        data: expect.any(String), // Verificando o formato da data enviada
+                        categoria: expect.any(String), // Verificando a categoria 'esperada'
+                        valor: '100',
+                        descricao: 'Descrição da despesa',
+                    }),
+                    headers: {
+                        'Content-type': 'application/json'
+                    }
+                })
+            );
+        });
     });
 });
